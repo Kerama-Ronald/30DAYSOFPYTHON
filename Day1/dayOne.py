0f1d4da600321ae073ec0529fe4904e267f58d0e
Ronald Kerama <keramaronald11@gmail.com>
2024-01-16 00:08:11 +0300

    Solution one

--- Day1/dayOne.py
+++ Day1/dayOne.py
@@ -5,4 +5,31 @@
 #  variables, data types, Math Operators, Comments, Input() function, Print() function, The str(), int(), and float() Functions
 
 # #### Exercise
-# thon CalcSimple Pyulator Program: Write a simple program that does basic math operations including addition, subtraction, deletion, division, power, squareroot,etc,. 
+#Python CalcSimple Pyulator Program: Write a simple program that does basic math operations including addition, subtraction, deletion, division, power, squareroot,etc,. 
+import math 
+input_one = int(input("Input Value 1 "))
+input_two = int(input("Input Value 2 "))
+sign = input("Input sign ")
+if sign == "+" :
+    sum = input_one + input_two
+    print (f"Answer is {sum} ")
+    
+elif sign == "*":
+    product = input_one * input_two
+    print ( f"Answer is {product}")
+
+elif  sign == "-" :
+    subtraction = input_one - input_two
+    print ( f"Answer is {subtraction}")
+    
+elif sign == "/" :
+    division = input_one / input_two
+    print ( f"Answer is {division}")
+
+elif sign == "**" :
+    power = input_one ** input_two
+    print ( f"Answer is {power}")
+    
+elif sign == "//" :
+    sqrt = math.sqrt(input_one)
+    print ( f"Answer is {sqrt}")
